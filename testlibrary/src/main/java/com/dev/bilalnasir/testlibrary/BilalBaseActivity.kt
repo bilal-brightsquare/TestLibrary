3b5debc2f7e4f00398c317b64a282789df1d64f1
bilaldev <bilal.brightsquare@gmail.com>
2024-08-18 23:33:55 +0500

    dummy library

--- testlibrary/src/main/java/com/dev/bilalnasir/testlibrary/BilalBaseActivity.kt
+++ testlibrary/src/main/java/com/dev/bilalnasir/testlibrary/BilalBaseActivity.kt
@@ -1,24 +1,13 @@
-package com.example.myapplication
+package com.dev.bilalnasir.testlibrary
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class BilalBaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
     }
 
     private fun DestroyAPP(message: String) {
@@ -36,7 +25,7 @@ class BilalBaseActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
             intent.putExtra(
                 Intent.EXTRA_TEXT,
-                "Download " + getString(R.string.app_name) + " now: https://play.google.com/store/apps/details?id=" + packagename
+                "Download : https://play.google.com/store/apps/details?id=$packagename"
             )
             intent.type = "text/plain"
             startActivity(intent)
--- testlibrary/src/main/java/com/dev/bilalnasir/testlibrary/MyUtill.kt
+++ testlibrary/src/main/java/com/dev/bilalnasir/testlibrary/MyUtill.kt
@@ -1,6 +1,6 @@
 package com.dev.bilalnasir.testlibrary
 
-class MyUtill {
+public class MyUtill {
     fun addnumbers(a:Int, b:Int): Int {
         return a + b
     }
